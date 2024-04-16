6ef2ebfad3cf326902676c4175981890617a8c90
ArinoMichi <cutesyprogrammer@gmail.com>
2024-04-16 09:23:48 +0200

    Azure Storage

---
+++ Proyectos/AzureStorage/MvcCoreAzureStorage/Services/ServiceStorageBlobs.cs
@@ -0,0 +1,80 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using MvcCoreAzureStorage.Models;
+
+namespace MvcCoreAzureStorage.Services
+{
+    public class ServiceStorageBlobs
+    {
+        private BlobServiceClient client;
+
+        public ServiceStorageBlobs(BlobServiceClient client)
+        {
+            this.client = client;
+        }
+
+        //METODO PARA MOSTRAR LOS CONTAINERS
+        public async Task<List<string>> GetContainersAsync()
+        {
+            List<string> containers = new List<string>();
+            await foreach (BlobContainerItem item in
+                this.client.GetBlobContainersAsync())
+            {
+                containers.Add(item.Name);
+            }
+            return containers;
+        }
+        //METODO PARA CREAR UN CONTENEDOR
+        public async Task CreateContainerAsync(string containerName)
+        {
+            await this.client.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
+        }
+        //METODO PARA ELIMINAR UN CONTENEDOR
+        public async Task DeleteContainerAsync(string containerName)
+        {
+            await this.client.DeleteBlobContainerAsync(containerName);
+        }
+
+        //METODO PARA MOSTRAR TODOS LOS BLOBS DE UN CONTAINER
+        public async Task<List<BlobModel>> GetBlobsAsync(string containerName)
+        {
+            //RECUPERAMOS EL CLIENT DEL CONTAINER
+            BlobContainerClient containerClient =
+                this.client.GetBlobContainerClient(containerName);
+            List<BlobModel> blobs = new List<BlobModel>();
+            await foreach (BlobItem item in containerClient.GetBlobsAsync())
+            {
+                //name, containerName, url
+                //DEBEMOS CREAR UN BLOBCLIENT SI NECESITAMOS
+                //TENER MAS INFORMACION DEL BLOB
+                BlobClient blobClient =
+                    containerClient.GetBlobClient(item.Name);
+                BlobModel blob = new BlobModel
+                {
+                    Nombre = item.Name,
+                    Contenedor = containerName,
+                    Url = blobClient.Uri.AbsoluteUri
+
+                };
+                blobs.Add(blob);
+            }
+            return blobs;
+        }
+
+        //METODO PARA ELIMINAR UN BLOB DE UN CONTAINER
+        public async Task DeleteBlobAsync(string containerName, string blobName)
+        {
+            BlobContainerClient containerClient =
+                this.client.GetBlobContainerClient(containerName);
+            await containerClient.DeleteBlobAsync(blobName);
+        }
+
+        //METODO PARA SUBIR UN BLOB A UN CONTAINER
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream stream)
+        {
+            BlobContainerClient containerClient =
+                this.client.GetBlobContainerClient(containerName);
+            await containerClient.UploadBlobAsync(blobName, stream);
+        }
+    }
+}
---
+++ Proyectos/AzureStorage/MvcCoreAzureStorage/Services/ServiceStorageFiles.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+
+namespace MvcCoreAzureStorage.Services
+{
+    public class ServiceStorageFiles
+    {
+        //TODO SERVICIO DE STORAGE SIEMPRE UTILIZA
+        //UN CLIENT PARA ACCEDER A SUS RECURSOS
+        //DICHO CLIENT NECESITA DE UNAS KEYS
+        private ShareDirectoryClient root;
+
+        //DEBEMOS RECIBIR LAS JETS DESDE APPSETTINGS
+        public ServiceStorageFiles(IConfiguration configuration)
+        {
+            string keys = configuration.GetValue<string>("AzureKeys:StorageAccount");
+            //CADA CLIENTE DE STORAGE ACCEDE A UN Share MEDIANTE 
+            //LAS CLAVES
+            ShareClient client =
+                new ShareClient(keys, "ejemplofiles");
+            this.root = client.GetRootDirectoryClient();
+        }
+
+        //METODO PARA RECUPERAR TODOS LOS FICHEROS DE LA 
+        //RAIZ DEL SHARED
+        public async Task<List<string>> GetFilesAsync()
+        {
+            List<string> files = new List<string>();
+            await foreach(ShareFileItem item in this.root.GetFilesAndDirectoriesAsync())
+            {
+                files.Add(item.Name);
+            }
+
+            return files;
+        }
+
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            //NECESITAMOS UN CLIENT DEL RECURSO QUE QUEREMOS
+            //RECUPERAR (file)
+            ShareFileClient file =
+                this.root.GetFileClient(fileName);
+            ShareFileDownloadInfo data= await file.DownloadAsync();
+            Stream stream = data.Content;
+            string contenido = "";
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                contenido = await reader.ReadToEndAsync();
+            }
+            return contenido;
+        }
+
+        public async Task UploadFileAsync(string fileName, Stream stream)
+        {
+            ShareFileClient file =
+                this.root.GetFileClient(fileName);
+            await file.CreateAsync(stream.Length);
+            await file.UploadAsync(stream);
+        }
+
+        public async Task DeleteFileAsync(string fileName)
+        {
+            ShareFileClient file =
+                this.root.GetFileClient(fileName);
+            await file.DeleteAsync();
+        }
+    }
+}
